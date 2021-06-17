a8735b22a3b03f7dddb7fe8476318c75f173406d
AlexeySvyatov <78646062+AlexeySvyatov@users.noreply.github.com>
2021-06-17 16:58:17 +0300

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,53 @@
 package com.company;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Random;
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter the number of rows: ");
-        int n = in.nextInt();
-        int[][] array = new int[n][n];
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                array[i][j] = 10 + (int) (Math.random() * 90);
+    public static void quickSort(int[] array, int low, int high){
+        if (array.length==0)
+            return;
+        if (low>=high)
+            return;
+        int middle = low + (high-low)/2;
+        int opora = array[middle];
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < opora){
+                i++;
+            }
+            while (array[j] > opora){
+                j--;
+            }
+            if (i <= j){
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
             }
         }
-        for (int i = 0; i < n; i++) {
-            System.out.println(Arrays.toString(array[i]));
+        if (low < j)
+            quickSort(array, low, j);
+        if (high > i)
+            quickSort(array, i, high);
+    }
+    public static void main(String[] args){
+        int[][] matrix = new int[5][5];
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                matrix[i][j] = new Random().nextInt(10);
+            }
         }
-        System.out.println("--------------------");
-        for(int i = 0; i < n; i++) {
-            Arrays.sort(array[i]);
-            System.out.println(Arrays.toString(array[i]));
+        System.out.println("Было");
+        for (int v = 0; v < 5; v++) {
+            System.out.println(Arrays.toString(matrix[v]));
+        }
+
+        int low = 0;
+        int high = matrix.length - 1;
+        System.out.println("Стало");
+        for (int h = 0; h < 5; h++) {
+            quickSort(matrix[h], low, high);
+            System.out.println(Arrays.toString(matrix[h]));
         }
     }
 }
